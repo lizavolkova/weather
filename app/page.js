807493b7da89ee83350eb3024f8b014da021f8cb
Liza Volkova <elvolkov@estee.com>
2023-07-21 21:03:13 -0400

    adds error catch to node api

--- app/page.js
+++ app/page.js
@@ -85,7 +85,7 @@ export default function Home() {
 
             const res = await fetch(`http://localhost:3000/api/coreData?real=${real}`)
             const data = await res.json();
-            
+
             setAirPollution(data.air.data);
             setData(data);
             setIsLoading(false);
