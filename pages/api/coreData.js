e1dff62bc291be8739957751b68996081fc529f2
Liza Volkova <elvolkov@estee.com>
2023-07-21 21:17:39 -0400

    reverts to old api, keeps node BE

--- pages/api/coreData.js
+++ pages/api/coreData.js
@@ -84,112 +84,112 @@ const fetchTomorrowWeather = async(real) => {
 export default async function handler(req, res) {
     try {
         const fetchRealData = req.query.real;
-        // const dataJson = await fetchOpenWeatherData(fetchRealData);
+        const dataJson = await fetchOpenWeatherData(fetchRealData);
 
         const air = await fetchAirDate(fetchRealData);
-        const tomorrowWeather = await fetchTomorrowWeather(fetchRealData);
+       // const tomorrowWeather = await fetchTomorrowWeather(fetchRealData);
 
-        const tomorrowHourly = tomorrowWeather.dailyHourly.timelines.hourly.slice(0,24);
+        //const tomorrowHourly = tomorrowWeather.dailyHourly.timelines.hourly.slice(0,24);
 
         const date = new Date()
         const dt = Math.floor(date.getTime() / 1000);
 
-        const daily =  tomorrowWeather.dailyHourly.timelines.daily.map(weather => {
-            const { values } = weather;
-            const date = new Date(weather.time);
-            const dt = Math.floor(date.getTime() / 1000);
-
-            const moonriseDate = new Date (values.moonriseTime);
-            const moonsetDate = new Date (values.moonsetTime);
-
-            const moonrise = Math.floor(moonriseDate.getTime() / 1000);
-            const moonset = Math.floor(moonsetDate.getTime() / 1000);
-
-
-            return {
-                dt,
-                cloud: values.cloudCoverAvg,
-                feels_like: {
-                    day: values.temperatureApparentMax,
-                    night: values.temperatureApparentMin,
-                    eve: values.temperatureApparentMin,
-                    morn: values.temperatureApparentMin,
-                },
-                humidity: values.humidityAvg,
-                pop: values.precipitationProbabilityAvg,
-                pressure: values.pressureSurfaceLevelAvg,
-                temp: {
-                    day: '',
-                    min: values.temperatureMin,
-                    max: values.temperatureMax,
-                    night: '',
-                    eve: '',
-                    morn: '',
-                },
-                uvi: values.uvIndexMax,
-                weather: [
-                    {
-                        id: 123,
-                        main: 'rain',
-                        icon: '10d',
-                        description: 'long description'
-                    }
-                ],
-                moonrise,
-                moonset,
-                moon_phase: ''
-            }
-        })
-
-
-        const { values } = tomorrowWeather.current.data;
-        const current = {
-            dt: dt,
-            feels_like: values.temperatureApparent,
-            temp: values.temperature,
-            uvi: values.uvIndex,
-            pop: values.precipitationProbability / 100,
-            clouds: values.cloudCover,
-            humidity: values.humidity,
-            pressure: values.pressureSurfaceLevel,
-            wind_speed: values.windSpeed,
-            weather: [
-                {
-                    description: 'current condition description',
-                    icon: '10d',
-                    id: tomorrowCurrent.id,
-                    main: 'clouds'
-                }
-            ],
-        };
-
-        const hourly = tomorrowHourly.map(weather => {
-            const date = new Date(weather.startTime)
-            const timestamp = Math.floor(date.getTime() / 1000);
-            return {
-                dt: timestamp,
-                feels_like: weather.temperatureApparent,
-                temp: weather.values.temperature,
-                uvi:weather.values.uvIndex,
-                pop: weather.values.precipitationProbability / 100,
-                clouds: weather.values.cloudCover,
-                humidity: weather.values.humidity,
-                weather: [
-                    {
-                        description: weather.description,
-                        icon: '10d',
-                        id: weather.id,
-                        main: weather.main
-                    }
-                ],
-            }
-        })
+        // const daily =  tomorrowWeather.dailyHourly.timelines.daily.map(weather => {
+        //     const { values } = weather;
+        //     const date = new Date(weather.time);
+        //     const dt = Math.floor(date.getTime() / 1000);
+        //
+        //     const moonriseDate = new Date (values.moonriseTime);
+        //     const moonsetDate = new Date (values.moonsetTime);
+        //
+        //     const moonrise = Math.floor(moonriseDate.getTime() / 1000);
+        //     const moonset = Math.floor(moonsetDate.getTime() / 1000);
+        //
+        //
+        //     return {
+        //         dt,
+        //         cloud: values.cloudCoverAvg,
+        //         feels_like: {
+        //             day: values.temperatureApparentMax,
+        //             night: values.temperatureApparentMin,
+        //             eve: values.temperatureApparentMin,
+        //             morn: values.temperatureApparentMin,
+        //         },
+        //         humidity: values.humidityAvg,
+        //         pop: values.precipitationProbabilityAvg,
+        //         pressure: values.pressureSurfaceLevelAvg,
+        //         temp: {
+        //             day: '',
+        //             min: values.temperatureMin,
+        //             max: values.temperatureMax,
+        //             night: '',
+        //             eve: '',
+        //             morn: '',
+        //         },
+        //         uvi: values.uvIndexMax,
+        //         weather: [
+        //             {
+        //                 id: 123,
+        //                 main: 'rain',
+        //                 icon: '10d',
+        //                 description: 'long description'
+        //             }
+        //         ],
+        //         moonrise,
+        //         moonset,
+        //         moon_phase: ''
+        //     }
+        // })
+        //
+        //
+        // const { values } = tomorrowWeather.current.data;
+        // const current = {
+        //     dt: dt,
+        //     feels_like: values.temperatureApparent,
+        //     temp: values.temperature,
+        //     uvi: values.uvIndex,
+        //     pop: values.precipitationProbability / 100,
+        //     clouds: values.cloudCover,
+        //     humidity: values.humidity,
+        //     pressure: values.pressureSurfaceLevel,
+        //     wind_speed: values.windSpeed,
+        //     weather: [
+        //         {
+        //             description: 'current condition description',
+        //             icon: '10d',
+        //             id: tomorrowCurrent.id,
+        //             main: 'clouds'
+        //         }
+        //     ],
+        // };
+        //
+        // const hourly = tomorrowHourly.map(weather => {
+        //     const date = new Date(weather.startTime)
+        //     const timestamp = Math.floor(date.getTime() / 1000);
+        //     return {
+        //         dt: timestamp,
+        //         feels_like: weather.temperatureApparent,
+        //         temp: weather.values.temperature,
+        //         uvi:weather.values.uvIndex,
+        //         pop: weather.values.precipitationProbability / 100,
+        //         clouds: weather.values.cloudCover,
+        //         humidity: weather.values.humidity,
+        //         weather: [
+        //             {
+        //                 description: weather.description,
+        //                 icon: '10d',
+        //                 id: weather.id,
+        //                 main: weather.main
+        //             }
+        //         ],
+        //     }
+        // })
 
 
         // PARSE OLD DATA
-        //const hourlyForecast = dataJson.hourly;
-        //const daily = dataJson.daily;
-        // const current = dataJson.current;
+        const hourly = dataJson.hourly;
+        const daily = dataJson.daily;
+        const current = dataJson.current;
 
         res.status(200).json({air, hourly, daily, current})
     } catch(err) {
