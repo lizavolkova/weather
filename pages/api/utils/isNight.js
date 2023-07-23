d3e2588a80089338e439f283e4105e5cc1ef900e
Liza Volkova <elvolkov@estee.com>
2023-07-23 17:06:00 -0400

    adds console log to isNight function

--- pages/api/utils/isNight.js
+++ pages/api/utils/isNight.js
@@ -1,4 +1,14 @@
 
 export default function isNight (date, rise, set) {
+    console.log('From isNight() function:')
+    console.log('Date', date)
+    console.log('Rise', date)
+    console.log('Set', date)
+    console.log('Date hours:', date.get())
+    console.log('Set hours:', set.get())
+    console.log('Rise hours:', rise.get())
+    console.log('date.getHours() > set.getHours()',date.getHours() > set.getHours())
+    console.log('date.getHours() < rise.getHours()',date.getHours() < rise.getHours())
+
     return date.getHours() > set.getHours() || date.getHours() < rise.getHours()
 }
