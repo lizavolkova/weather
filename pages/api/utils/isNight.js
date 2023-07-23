9be89be9d07d5011012c26ade1fbde9f7f045fc6
Liza Volkova <elvolkov@estee.com>
2023-07-23 17:08:41 -0400

    fixes stupid date.get() typo

--- pages/api/utils/isNight.js
+++ pages/api/utils/isNight.js
@@ -4,9 +4,9 @@ export default function isNight (date, rise, set) {
     console.log('Date', date)
     console.log('Rise', date)
     console.log('Set', date)
-    console.log('Date hours:', date.get())
-    console.log('Set hours:', set.get())
-    console.log('Rise hours:', rise.get())
+    console.log('Date hours:', date.getHours())
+    console.log('Set hours:', set.getHours())
+    console.log('Rise hours:', rise.getHours())
     console.log('date.getHours() > set.getHours()',date.getHours() > set.getHours())
     console.log('date.getHours() < rise.getHours()',date.getHours() < rise.getHours())
 
